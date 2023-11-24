ec09007deadfebd3f331a7a2bdafcb6e7306716d
Heyner <89066219+leiva2021@users.noreply.github.com>
2023-11-23 21:44:49 -0600

    Fixed error of multiple primary key

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email')->index();
+            $table->foreign('email')->references('email')->on('users')->onDelete('cascade');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
